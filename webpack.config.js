f613a460a811cd0de2ad80a70361f6d8ec0ae498
Steven Inouye <steveninouye@msn.com>
2019-01-14 11:04:58 -0800

    Install node modules, add gitignore file, add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/App.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client', 'dist'),
+    filename: 'bundle.js',
+    publicPath: path.resolve('/')
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/, /\.js$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['babel-plugin-styled-components']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpeg|jpg|png|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/src/index.html',
+      inject: 'body'
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'client', 'dist'),
+    port: 3000,
+    compress: true,
+    open: true,
+    stats: 'errors-only',
+    historyApiFallback: true,
+    proxy: [{ context: ['/'], target: 'http://localhost:8080' }]
+  }
+};
